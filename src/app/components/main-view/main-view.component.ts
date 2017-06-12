10bca09737dcd244f72d1c3af17a92920810df64
HMLong <hominhlong.it@gmail.com>
2017-06-12 15:29:41 +0700

    Add basic process for get data from API Gateway

--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -7,26 +7,60 @@ import 'rxjs/Rx';
 import { CognitoAuthService, LoggedInCallback } from './../../services/cognito-auth.service';
 
 @Component({
-    selector: 'app-main',
-    templateUrl: 'main-view.template.html'
+  selector: 'app-main',
+  templateUrl: 'main-view.template.html'
 })
 
-export class MainViewComponent implements LoggedInCallback {
-    private commentsUrl;
+export class MainViewComponent implements LoggedInCallback, OnInit {
+  apiURL: any = 'https://q22f35kxa7.execute-api.us-east-1.amazonaws.com/dev/locations'
+  token: any
+  locationData: any
 
-    constructor(
-        public router: Router,
-        public userService: CognitoAuthService,
-        private http: Http
-    ) {
-        // Checking if the user is already authenticated.
-        // If so, then redirect to the secure site
-        this.userService.isAuthenticated(this);
-    }
+  constructor(
+    public router: Router,
+    public authService: CognitoAuthService,
+    private _http: Http
+  ) {
+    // Checking if the user is already authenticated.
+    // If so, then redirect to the secure site
+    this.authService.isAuthenticated(this);
+  }
 
-    isLoggedIn(message: string, isLoggedIn: boolean) {
-        if (!isLoggedIn) {
-            this.router.navigate(['/login']);
-        }
+  ngOnInit() {
+    const userPool = this.authService.getUserPool();
+    const cognitoUser = userPool.getCurrentUser();
+
+    cognitoUser.getSession((err, session) => {
+      this.token = session.getIdToken().getJwtToken()
+
+      this.getInfoApiUserPools(this.token).subscribe((data) => {
+        this.locationData = data
+      });
+    });
+  }
+
+  isLoggedIn(message: string, isLoggedIn: boolean) {
+    if (!isLoggedIn) {
+      this.router.navigate(['/login']);
     }
+  }
+
+  getInfoApiUserPools(token): Observable<any> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Authorization', token);
+
+    return this._http.get(this.apiURL, { headers: headers })
+      .map(res => res.json())
+      .catch(this._serverError);
+  }
+
+  private _serverError(err: any) {
+    if (err.status === 0) {
+      return Observable.throw(err.json().error || 'UNAUTHORIZED!!!');
+    }
+    if (err instanceof Response) {
+      return Observable.throw(err.json().error || 'Backend Server Error');
+    }
+  }
 }
