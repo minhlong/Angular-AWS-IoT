b11ff31108cc2546ecba8294ae6cfa7b365fe110
HMLong <hominhlong.it@gmail.com>
2017-06-18 12:42:03 +0700

    Update readme

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs/Subscription';
 
 import { AuthSelector } from '../../store/auth.selector';
 import { AppState } from '../../store/reducers/index';
@@ -10,21 +11,23 @@ import { AuthActions } from './../../store/actions/auth.action';
   selector: 'app-login-component',
   templateUrl: 'login.template.html'
 })
-
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
   login: { email?: string, password?: string } = {};
   isLoading = false;
   errorMessage: string;
+
+  private handleLoading: Subscription;
+  private handleErr: Subscription;
 
   constructor(
     private authActions: AuthActions,
     private store: Store<AppState>
   ) {
-    this.store.let(AuthSelector.isLoading()).subscribe(res => {
+    this.handleLoading = this.store.let(AuthSelector.isLoading()).subscribe(res => {
       this.isLoading = res
     });
 
-    this.store.let(AuthSelector.getErrorMessage()).subscribe(err => {
+    this.handleErr = this.store.let(AuthSelector.getErrorMessage()).subscribe(err => {
       this.errorMessage = err
     });
   }
@@ -36,5 +39,10 @@ export class LoginComponent {
     if (form.valid) {
       this.store.dispatch(this.authActions.auth(this.login.email, this.login.password));
     }
+  }
+
+  ngOnDestroy() {
+    this.handleLoading.unsubscribe();
+    this.handleErr.unsubscribe();
   }
 }
--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { environment } from '../../../environments/environment.prod';
 import { JwtAuthHttp } from './../../services/http-auth.service';
 
+declare const AWS: any;
+
 @Component({
   selector: 'app-main',
   templateUrl: 'main-view.template.html'
@@ -14,14 +16,15 @@ export class MainViewComponent {
 
   apiIoT: String = 'a243uabiez3zv6.iot.us-east-1.amazonaws.com'
   thingName: String = 'HMLong-Thing1'
-  iotPolicy: String = 'HMLong-Policy1'
   thingShadow: any = '{ "state": { "desired" : { "color" : { "r" : 10 }, "engine" : "ON" } } }'
   iotData: any
+  private ioTShadow: any
 
   constructor(
     private _http: JwtAuthHttp
   ) {
     this.getLocations();
+    this.initIoTShadow();
   }
 
   private getLocations() {
@@ -31,5 +34,50 @@ export class MainViewComponent {
       .subscribe(data => {
         this.locationData = data
       });
+  }
+
+  initIoTShadow() {
+    // Update AWS token for use in another services
+    const cognitoParams = {
+      IdentityPoolId: environment.identityPoolId,
+      Logins: {}
+    };
+    cognitoParams.Logins[
+      'cognito-idp.' + environment.region +
+      '.amazonaws.com/' + environment.userPoolId
+    ] = localStorage.getItem('token');
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials(cognitoParams);
+
+    // Get Data Shadow
+    this.ioTShadow = new AWS.IotData({
+      endpoint: this.apiIoT
+    });
+    const that = this
+    this.ioTShadow.getThingShadow({ thingName: this.thingName }, function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        that.iotData = data
+      }
+    });
+  }
+
+  /**
+   * Update IoT Shadow
+   * Ref https://stackoverflow.com/questions/40104559/forbidden-exception-on-accessing-aws-iot-using-amazon-cognito
+   */
+  updateIoT() {
+    const options = {
+      thingName: this.thingName,
+      payload: this.thingShadow
+    };
+    const that = this
+    this.ioTShadow.updateThingShadow(options, function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        that.iotData = data
+      }
+    });
   }
 }
